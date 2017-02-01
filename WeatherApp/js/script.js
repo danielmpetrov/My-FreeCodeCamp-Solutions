9fc54599e8772792044529271d8b6db06d70b700
Daniel Petrov <daniel221569@gmail.com>
2017-02-01 21:21:48 +0100

    restructuring

--- WeatherApp/js/script.js
+++ WeatherApp/js/script.js
@@ -1,53 +1,43 @@
 const API_KEY = 'dccbf5e723c2f71607f4a80bdf256bc7';
-const htmlCelsius = '&#8451;';
-
-function kelToCel(k) {
-  return (k - 273.15).toFixed(1);
-}
 
 function handleError(xhr, state, error) {
-  console.error(arguments);
+  $("#message").html("Whoops... an error :(");
 }
 
 function weatherSuccess(data) {
-  var temperature = data.main.temp; // kelvin
-  var theWeather = data.weather[0].main;
-  var description = data.weather[0].description;
-  var country = data.sys.country;
-  var city = data.name;
-  var icon = data.weather[0].icon;
-  var iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png';
-
-  $('span.weather-description').html(theWeather + ' ( ' + description + ' )');
-  $('span.country').html('Country: ' + country);
-  $('span.city').html('City of ' + city);
-  $('span.temperature').html(kelToCel(temperature) + htmlCelsius);
-  $('#icon').attr('src', iconUrl);
+  data.main.temp = data.main.temp.toFixed(1); // round the temp
+  var source = $("#weather-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(data);
+  $("#message").html(html);
 }
 
 function displayData() {
-    var urlByCity = function(city) {
-      var base = 'http://api.openweathermap.org/data/2.5/weather?q=';
-      return base + city + '&appid=' + API_KEY;
-    };
+  var urlByCity = function(city) {
+    var base = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    return base + city + '&appid=' + API_KEY + '&units=metric';
+  };
 
-    var userCity = $('#userCity').val();
-    $.get(urlByCity(userCity)).then(weatherSuccess, handleError);
-  }
+  var $cityInput = $('#userCity');
+  var userCity = $cityInput.val();
+  $cityInput.val('');
+  $.get(urlByCity(userCity)).then(weatherSuccess, handleError);
+}
 
 $(document).ready(function() {
   $.get('http://ip-api.com/json').then(function(ipJSON) {
     var lat = ipJSON.lat;
     var lon = ipJSON.lon;
     var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
-      lat + '&lon=' + lon + '&appid=' + API_KEY;
+      lat + '&lon=' + lon + '&appid=' + API_KEY + '&units=metric';
     return $.get(requestUrl);
   }).then(weatherSuccess, handleError);
   
   $('#getWeather').click(displayData);
   
   $('#userCity').keypress(function(e) {
-    if (e.which == 13)
-        displayData();
+    if (e.which == 13) {
+      displayData();
+    }
   });
 });
